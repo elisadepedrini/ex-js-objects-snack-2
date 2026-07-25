072a958cf4964d80486bda428114703c783303a6
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-25 10:59:25 +0200

    Complete code question 1

--- code-question-1.js
+++ code-question-1.js
@@ -3,9 +3,10 @@ const secondBurger = hamburger;
 secondBurger.name = 'Double Cheese Burger';
 secondBurger.weight = 500;
 
-console.log(hamburger.name); // ?
-console.log(secondBurger.name ); // ?
-
-
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name ); // Double Cheese Burger
+
+
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// Uno solo
