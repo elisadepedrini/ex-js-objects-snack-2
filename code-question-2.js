1775a51e82fec4491a4c4f4f4a2c166832236f09
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-26 10:03:45 +0200

    Complete code question 2

--- code-question-2.js
+++ code-question-2.js
@@ -7,10 +7,12 @@ const hamburger = {
 const secondBurger = {...hamburger};
 secondBurger.ingredients[0] = "Salad";
 
-console.log(hamburger.ingredients[0]); // ?
-console.log(secondBurger.ingredients[0]); // ?
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
 
 // P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
 
-// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// 2 oggetti, in quanto secondBurger non fa riferimento all'oggetto burger, ma crea un nuovo oggetto, uguale, utilizzando l'operatore spread
+
